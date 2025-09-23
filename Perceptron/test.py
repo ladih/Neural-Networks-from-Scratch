3665d9f9997ddbd01fdf608f84c4750908ce2eda
ladih <138324910+ladih@users.noreply.github.com>
2025-09-23 19:08:24 +0200

    Update test.py

--- Perceptron/test.py
+++ Perceptron/test.py
@@ -1,3 +1,5 @@
+# Testing the perceptron on two clusters of points in the plane
+
 import perceptron as perc
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,35 +10,42 @@ def generate_gaussian_points(mean, cov, num_points):
     points = np.random.multivariate_normal(mean, cov, num_points)
     return points
 
-# specify first cluster
+# Mean and covariance of first cluster of training samples
 mean1 = [0, 0]
 cov1 = [[0.2, 0], [0, 0.2]]
 
-# specify second cluster
+# Mean and covariance of second of training samples
 mean2 = [3, 3]
 cov2 = [[0.2, 0], [0, 0.2]]
 
+# Number of points in each cluster
 num_points = 10
+
+# Ones for input format (x1, x2, 1)
 ones = np.ones((num_points, 1))
 
-# Generate two clusters of points that are linearly separable
+# Generate two clusters of points that are (hopefully) linearly separable
 points1 = generate_gaussian_points(mean1, cov1, num_points)
-points1 = np.hstack((points1, ones))       # extend points from (x1, x2) to (x1, x2, 1) (in order to be compatible with the weight vector (w1, w2, w3))
+points1 = np.hstack((points1, ones)) # (x1, x2) -> (x1, x2, 1)
 
 points2 = generate_gaussian_points(mean2, cov2, num_points)
 points2 = np.hstack((points2, ones))
 
+
+# Labels for training data
 ones   = np.ones(num_points)
 zeros  = np.zeros(num_points)
 labels = np.concatenate((ones, zeros))
 
+# Label the clusters
 training_data = np.concatenate((points1, points2), axis=0)
 
-p.train(training_data, labels)  # train the perceptron on the training data
+# Train the perceptron
+p.train(training_data, labels)
 
-# Plot evolution of the decision boundaries
+# Plot evolution of decision boundaries
 n_updates = len(p.weightsequence)
-x = np.linspace(-5, 5, 1000)
+x = np.linspace(-5, 5, 100)
 
 color = iter(plt.cm.rainbow(np.linspace(0, 1, n_updates)))
 for i in range(n_updates-1):
@@ -49,7 +58,7 @@ for i in range(n_updates-1):
        plt.plot(x, y, c=c, label=str(i+1))
 
 y = (-p.weights[2]-p.weights[0] * x) / p.weights[1]
-plt.plot(x, y, c='blue', linewidth=3, label='Last boundary')
+plt.plot(x, y, c='blue', linewidth=3, label='Final boundary')
 
 
 for i in range(num_points):
@@ -61,6 +70,6 @@ plt.xlim(-3, 6)  # Set x-axis limits
 plt.ylim(-3, 6)  # Set y-axis limits
 plt.legend()
 plt.title("Evolution of decision boundaries during training")
-plt.xlabel("input feature 1")
-plt.ylabel("input feature 2")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
 plt.show()
