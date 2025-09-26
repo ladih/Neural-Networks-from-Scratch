7126281c0bf8208a7e8553d7297482b1455cc6e6
ladih <138324910+ladih@users.noreply.github.com>
2025-09-26 16:15:29 +0200

    Update test.py

--- Backpropagation/test.py
+++ Backpropagation/test.py
@@ -7,32 +7,12 @@ y = np.array([[1], [1], [1], [1], [1], [0], [0], [0]])     # labels
 
 nn = NeuralNetwork(input_size=len(X[0]), output_size=len(y[0]), hidden_sizes=[2, 2])
 
-nn.train(X, y, learning_rate=0.1, n_epochs=20000, batch_size=len(X), error_threshold=0.01)
+error_threshold = 1e-3 # training stops if
+n_batches = 20000 # number of batches trained on
+batch_size = 8       # must be <= len(X)
+nn.train(X, y, learning_rate=0.1, n_batches=n_batches, batch_size=batch_size, error_threshold=error_threshold)
 
 nn.plot_error_curve()
-
-def plot_2D(n):
-    """If input is 2D and output is 1D, this function shows how n
-        random points in the plane are classified (assuming labels are 0 or 1)"""
-    xmin, xmax = -0.5, 1.5
-    ymin, ymax = -0.5, 1.5
-
-    # dummy points for legend
-    plt.scatter([], [], color='red', label='Class 1')
-    plt.scatter([], [], color='black', label='Class 0')
-
-    for i in range(n):
-        x = random.uniform(xmin, xmax)
-        y = random.uniform(ymin, ymax)
-        res = nn.predict(np.array([[x, y]]))
-        if res > 0.5:
-            plt.scatter(x, y, color='red')
-        else:
-            plt.scatter(x, y, color='black')
-    plt.title("Example of how points in the plane are classified")
-    plt.xlabel("input feature 1")
-    plt.ylabel("input feature 2")
-    plt.show()
 
 # Plot training data
 # Class 1 points
@@ -47,7 +27,30 @@ plt.xlabel("input feature 1")
 plt.ylabel("input feature 2")
 plt.show()
 
-# plot how points are classified after training
-plot_2D(500)
 
 
+def classifications_2d(n_points, nn):
+    """Shows classification of n_points random points in the plane.
+        Only works if input = 2d, output = 1d"""
+    xmin, xmax = -0.5, 1.5
+    ymin, ymax = -0.5, 1.5
+
+    # dummy points for legend
+    plt.scatter([], [], color='red', label='Class 1')
+    plt.scatter([], [], color='black', label='Class 0')
+
+    for i in range(n_points):
+        x = random.uniform(xmin, xmax)
+        y = random.uniform(ymin, ymax)
+        res = nn.predict(np.array([[x, y]]))
+        if res > 0.5:
+            plt.scatter(x, y, color='red')
+        else:
+            plt.scatter(x, y, color='black')
+    plt.title("Example of how points in the plane are classified")
+    plt.xlabel("input feature 1")
+    plt.ylabel("input feature 2")
+    plt.show()
+
+# show classifications in the plane
+classifications_2d(n_points = 500, nn=nn)
